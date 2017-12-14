3b39b231ba8bc40ffd7aeb93c0fa92fd0af114f6
jesper0438 <robr0002@hz.nl>
2017-12-14 12:43:43 +0100

    Able to get data in VueJS #15

--- resources/assets/js/components/Hzapi.vue
+++ resources/assets/js/components/Hzapi.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hello">
-        <h1>Your IP is {{ academie_id }}</h1>
+        <h1>Your IP is {{ Academie_Id.hzopleiding_code }}</h1>
     </div>
 </template>
 
@@ -11,13 +11,13 @@
         name: 'HelloWorld',
         data () {
             return {
-                academie_id: []
+                Academie_Id: []
             }
         },
         mounted() {
             this.$http.get("https://apps.hz.nl/Services/algemeen/v1/crohos/34139/opleidingsvarianten").then(result => {
-                this.academie_id = result.data.origin;
-                console.log(result.data);
+                this.Academie_Id = result.data[0];
+                console.log(this.Academie_Id.hzopleiding_code);
             }, error => {
                 console.error(error);
             });
