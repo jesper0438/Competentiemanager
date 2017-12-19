af3991e747049b1abb6ddee84d03d596b7da9acf
jesper0438 <robr0002@hz.nl>
2017-12-19 09:37:36 +0100

    Added some comments

--- tests/Feature/StudentTest.php
+++ tests/Feature/StudentTest.php
@@ -72,15 +72,31 @@ class StudentTest extends TestCase
         $student = factory(\App\Student::class)->create(['amountec' => 'genoeg']);
     }
 
-    public function test_currentproject_ec_is_required()
+    /**
+     * A test to check if the amount-ec field is required.
+     * @return true
+     */
+
+    public function test_if_currentproject_is_required()
     {
         $this->assertTrue(true);
         $student = factory(\App\Student::class)->create(['currentproject' => 'De competentiemanager']);
     }
 
-    public function test_currentcompetenties_ec_is_required()
+    /**
+     *
+     * A test to check if the currentproject is required
+     * @return true
+     */
+
+    public function test_if_currentcompetenties_is_required()
     {
         $this->assertTrue(true);
         $student = factory(\App\Student::class)->create(['currentcompetenties' => 'SRE2A']);
     }
+    /**
+     * A test to check if the currentcompetenties is required
+     * @return true
+     */
+
 }
